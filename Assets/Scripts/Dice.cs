4ac9464f706623af49dda9c98cd645d9cc7db12f
Mario Toledo <mariotoledo12@gmail.com>
2022-07-17 14:06:11 -0300

    adding throwing system

---
+++ Assets/Scripts/Dice.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dice : MonoBehaviour
+{
+    public void Toss(Vector3 direction) {
+        Rigidbody rigidBody = GetComponent<Rigidbody>();
+        Vector3 diceVelocity = rigidBody.velocity;
+
+        float directionX = Random.Range(0, 500);
+        float directionY = Random.Range(0, 500);
+        float directionZ = Random.Range(0, 500);
+
+        rigidBody.AddForce((direction - transform.position) * 200);
+        rigidBody.AddTorque(directionX, directionY, directionZ);
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public Dice dicePrefab;
+    public Button tossButton;
+
+    public GameObject directionIndicator;
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tossButton.onClick.AddListener(TossDice);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void TossDice() {
+        Dice dice = Instantiate(dicePrefab, player.transform.position, Quaternion.identity);
+        Dice dice2 = Instantiate(dicePrefab, player.transform.position, Quaternion.identity);
+        Dice dice3 = Instantiate(dicePrefab, player.transform.position, Quaternion.identity);
+        Debug.Log(directionIndicator.transform.position);
+        dice.Toss(directionIndicator.transform.position);
+        dice2.Toss(directionIndicator.transform.position);
+        dice3.Toss(directionIndicator.transform.position);
+    }
+}
